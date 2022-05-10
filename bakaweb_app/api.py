affc4b86bfd6b52e511888358c4c091163a7cab2
bakterio <lukastoman594@gmail.com>
2022-05-10 23:03:51 +0200

    api implementation - login, znamky

--- bakaweb_app/api.py
+++ bakaweb_app/api.py
@@ -2,7 +2,7 @@ import requests
 
 def get_token(url: str, username: str, pw: str):
     r = requests.post(
-        url=url,
+        url=url + '/api/login',
         headers={"Content-Type": "application/x-www-form-urlencoded"},
         data=f"client_id=ANDR&grant_type=password&username={username}&password={pw}")
 
@@ -11,7 +11,7 @@ def get_token(url: str, username: str, pw: str):
 
 def znamky(url: str, token: str):
     r = requests.get(
-        url=url,
+        url=url + '/api/3/marks',
         headers={
             "Content-Type": "application/x-www-form-urlencoded",
             'Authorization': 'Bearer ' + token
--- bakaweb_app/views.py
+++ bakaweb_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from . import api
 
 # Create your views here.
-def test(request):
-    return render(request, 'bakaweb_app/index.html')
+class IndexView(generic.TemplateView):
+    template_name = 'bakaweb_app/index.html'
+
+    def post(self, request):
+        input = request.POST
+        token = api.get_token(input['url'], input['user'], input['pw'])
+        data = api.znamky(input['url'], token)
+        #   return render(request, self.template_name, {"out": api.znamky(input['url'], token)})
+        return HttpResponse(data)
