440f75f40bf2deb8e3c6534a3673fa7830fb7fb6
bakterio <lukastoman594@gmail.com>
2022-05-18 23:09:33 +0200

    User model class

--- bakaweb_app/models.py
+++ bakaweb_app/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    url = models.URLField(null=False)
+    user_name = models.CharField(max_length=512)
+    pw = models.CharField(max_length=512)
+    first_login = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_name
