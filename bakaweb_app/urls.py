340c09462cf232f410e0099b9c4265eff7bfb21b
bakterio <lukastoman594@gmail.com>
2022-05-20 22:33:38 +0200

    automatic reloading routing error fixed

--- bakaweb_app/urls.py
+++ bakaweb_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', IndexView.as_view(), 'baka'),
+    path('', IndexView.as_view(), name='baka'),
     path('test/', test, name='test'),
     path('marks/', marks, name='marks'),
     path('timetable/', timetable, name='timetable'),
--- bakaweb_app/views.py
+++ bakaweb_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views import generic
+from django.urls import reverse
+from requests.sessions import session
 from .api import BakConnection
 from .forms import *
 
-# Create your views here.
+def check_session(request):
+    for i in ('url', 'user', 'pw'):
+        if request.session[i] == None:
+            return None
+
 class IndexView(generic.TemplateView):
     template_name = 'bakaweb_app/index.html'
 
@@ -16,12 +22,19 @@ class IndexView(generic.TemplateView):
 
             return redirect('/marks')
 
+    def delete(self, request):
+        for i in ('url', 'user', 'pw'):
+            request.session[i] = None
+
 def test(request):
     form = LoginForm(request.POST or None)
     return render(request, 'bakaweb_app/test.html', {'form': form})
 
 def marks(request):
+    if check_session(request) == None:
+        redirect(reverse('baka'))
     return render(request, 'bakaweb_app/marks.html', {'subjects': BakConnection(request.session).marks()})
 
 def timetable(request):
     return render(request, 'bakaweb_app/timetable.html', {'timetable': BakConnection(request.session).timetable()})
+
