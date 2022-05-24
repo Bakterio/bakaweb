9e59fd343b00e7d30a68b230aa27d91986aa49d8
bakterio <lukastoman594@gmail.com>
2022-05-24 19:43:58 +0200

    navbar - backend

--- bakaweb_app/views.py
+++ bakaweb_app/views.py
@@ -1,7 +1,8 @@
-from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse
+from django.core.mail import send_mail
 from .api import BakConnection
 from .forms import *
 
@@ -32,11 +33,13 @@ def marks(request):
     return render(request, 'bakaweb_app/marks.html', {'subjects': BakConnection(request.session).marks()})
 
 def timetable(request):
+    if check_session(request):
+        return redirect(reverse('baka'))
     return render(request, 'bakaweb_app/timetable.html', {'timetable': BakConnection(request.session).timetable()})
 
 def logout(request):
     if check_session(request):
-        return HttpResponse('alfjalkjdjf')
+        return redirect(reverse('baka'))
     for i in ('url', 'user', 'pw'):
         del request.session[i]
     return redirect(reverse('baka'))
