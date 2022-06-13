71e37b5f8da66dcb22ee9195df392fbd64f97d64
stepan <stepan.hofmeister7@protonmail.com>
2022-06-13 17:51:52 +0200

    nefunkcni_zmena_designu

--- bakaweb_app/static/bakaweb_app/js/script.js
+++ bakaweb_app/static/bakaweb_app/js/script.js
@@ -1,9 +1,20 @@
-const ShowPasswordCheckbox = document.getElementById("password-input");
+var ShowPasswordCheckbox = document.getElementById("password-input")
+var PasswordEye = document.getElementById("PasswordEye")
+var SlashPasswordEye = document.getElementById("SlashPasswordEye")
+var idk = document.getElementsByClassName("fa-eye")
+var idk2 = document.getElementsByClassName("fa-eye-slash")
 
 function ZmenTypInputu() {
     if (ShowPasswordCheckbox.type === "password") {
         ShowPasswordCheckbox.type = "text";
+        console.log("1")
+            // idk.style.display = "block";
+            // idk2.style.display = "none";
+        idk.classList.add('displaynone')
+        console.log("2")
     } else {
         ShowPasswordCheckbox.type = "password";
+        // idk.style.display = "none";
+        // idk2.style.display = "block";
     }
 }
