1cc89080c912f19e6e95f9395505de17047bd0f5
bakterio <lukastoman594@gmail.com>
2022-06-13 19:31:12 +0200

    design by Stěpán Hofmaister

--- bakaweb_app/static/bakaweb_app/js/script.js
+++ bakaweb_app/static/bakaweb_app/js/script.js
@@ -7,10 +7,3 @@ function ZmenTypInputu() {
         ShowPasswordCheckbox.type = "password";
     }
 }
-
-function logout() {
-    s = NewSession();
-    s.Item('user') = null;
-    s.Item('url') = null;
-    s.Item('pw') = null;
-}
